dd50e27e5d0219402bd4f8f96d7e28b404d5c7aa
Yannis Jaquet <yannis.jaquet@unige.ch>
2013-09-19 12:13:52 +0200

    corrects the god config

--- config/start_god_staging.sh
+++ config/start_god_staging.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /Users/yannis/railsapps/biology14_staging/current;
 /Users/yannis/.rbenv/shims/god;
-/Users/yannis/.rbenv/shims/god load config/unicorn.god && /Users/yannis/.rbenv/shims/god start biology14_staging_unicorn;
+/Users/yannis/.rbenv/shims/god load config/unicorn_staging.god && /Users/yannis/.rbenv/shims/god start biology14_staging_unicorn;

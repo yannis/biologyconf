809b64a5c4a326884e9016d9dc406db407840939
yannis <yannisjaquet@me.com>
2013-12-19 16:15:37 +0100

    restrict the countries list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
     @registration = Registration.new(category_name: "non_member", country: "Switzerland")
     @grouped_events = Event.order(:start).group_by{|e| e.start.to_date}
     @speakers = Speaker.order(:last_name, :first_name)
+    @countries = (Country.find_all_countries_by_subregion('Northern America')+Country.find_all_countries_by_region('Europe')).uniq.sort_by(&:name)
   end
 end
